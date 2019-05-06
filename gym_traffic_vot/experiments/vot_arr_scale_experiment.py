a8e99b80294dfd1e51ddef1abb43fe853b489fdf
BCAI Haifa <bcai.haifa@gmail.com>
2019-05-06 19:11:47 +0300

    add exp

--- gym_traffic_vot/experiments/vot_arr_scale_experiment.py
+++ gym_traffic_vot/experiments/vot_arr_scale_experiment.py
@@ -16,69 +16,73 @@ confidence = 0.95
 
 def main(args):
     # configure logger, disable logging in child MPI processes (with rank > 0)
+    for a in np.arange(0.01, 1.1, 0.01):
+        for s in [1,2,4,8]:
 
-    arg_parser = run.common_arg_parser()
-    args, unknown_args = arg_parser.parse_known_args(args)
-    extra_args = run.parse_cmdline_kwargs(unknown_args)
-    save_path = 'C:/Users/FEZ1TV/PycharmProjects/gym-traffic-vot/models/test'
+            arg_parser = run.common_arg_parser()
+            args, unknown_args = arg_parser.parse_known_args(args)
+            extra_args = run.parse_cmdline_kwargs(unknown_args)
+            save_path = '~/projects/traffic-gym-vot/models/vot_{}_{}_deepq_'.format(a, s)
+            res_path =  '~/projects/traffic-gym-vot/resutls/vot_{}_{}_deepq_'.format(a, s)
+            env_name = 'traffic-vot-simple-v0'
 
-    env_name = 'traffic-vot-simple-v0'
+            env = gym.make(env_name, network='simple', arrival_rate=a, scale=s)
+            if args.network:
+                extra_args['network'] = args.network
+            else:
+                if extra_args.get('network') is None:
+                    extra_args['network'] = 'mlp'
 
-    env = gym.make(env_name, network='simple', arrival_rate=0.1, scale=4)
-    if args.network:
-        extra_args['network'] = args.network
-    else:
-        if extra_args.get('network') is None:
-            extra_args['network'] = 'mlp'
+            print('Training {} on {} with arguments \n{}'.format(args.alg, env_name, extra_args))
 
-    print('Training {} on {} with arguments \n{}'.format(args.alg, env_name, extra_args))
+            learn = run.get_learn_function(args.alg)
+            model = learn(
+                env=env,
+                seed=args.seed,
+                total_timesteps=int(args.num_timesteps),
+                **extra_args
+            )
 
-    learn = run.get_learn_function(args.alg)
-    model = learn(
-        env=env,
-        seed=args.seed,
-        total_timesteps=int(args.num_timesteps),
-        **extra_args
-    )
+            save_path = osp.expanduser(save_path)
+            model.save(save_path)
 
-    save_path = osp.expanduser(save_path)
-    model.save(save_path)
-
-    logger.log("Running trained model")
-    obs = env.reset()
-
-    state = model.initial_state if hasattr(model, 'initial_state') else None
-    dones = np.zeros((1,))
-
-    episode_rew = 0
-    episode_rewards = []
-    while True:
-        if state is not None:
-            actions, _, state, _ = model.step(obs,S=state, M=dones)
-        else:
-            actions, _, _, _ = model.step(obs)
-
-        obs, rew, done, _ = env.step(actions)
-        episode_rew += rew
-        env.render()
-        done = done.any() if isinstance(done, np.ndarray) else done
-        if done:
-            episode_rewards.append(episode_rew)
-            print('episode_rew={}'.format(episode_rew))
-            episode_rew = 0
-            if 0 == len(episode_rewards) % 10:
-                n = len(episode_rewards)
-                m = mean(episode_rewards)
-                std_err = sem(episode_rewards)
-                h = std_err * t.ppf((1 + confidence) / 2, n - 1)
-                print('Avg. reward of last {} episodes is [{:.2f}-{:.2f}]'.format(n, m - h, m + h))
-                if h < abs(m * 0.01):
-                    break
+            logger.log("Running trained model")
             obs = env.reset()
 
-    env.close()
+            state = model.initial_state if hasattr(model, 'initial_state') else None
+            dones = np.zeros((1,))
 
-    return model
+            episode_rew = 0
+            episode_rewards = []
+            f = open(osp.expanduser(res_path), 'w')
+            while True and len(episode_rewards) < 1000:
+                if state is not None:
+                    actions, _, state, _ = model.step(obs,S=state, M=dones)
+                else:
+                    actions, _, _, _ = model.step(obs)
+
+                obs, rew, done, _ = env.step(actions)
+                episode_rew += rew
+                env.render()
+                done = done.any() if isinstance(done, np.ndarray) else done
+                if done:
+                    episode_rewards.append(episode_rew)
+                    f.write('episode_rew={}'.format(episode_rew), f)
+                    episode_rew = 0
+                    if 0 == len(episode_rewards) % 10:
+                        n = len(episode_rewards)
+                        m = mean(episode_rewards)
+                        std_err = sem(episode_rewards)
+                        h = std_err * t.ppf((1 + confidence) / 2, n - 1)
+                        f.write('Avg. reward of last {} episodes is [{:.2f}-{:.2f}]'.format(n, m - h, m + h))
+                        if h < abs(m * 0.01):
+                            break
+                    obs = env.reset()
+
+            f.close()
+            env.close()
+
+            return model
 
 if __name__ == '__main__':
     main(sys.argv)
